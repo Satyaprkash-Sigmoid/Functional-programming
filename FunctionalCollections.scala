87543cee17d83e1aa9cf958df89c421baa91fe9a
Satyaprkash-Sigmoid <satyaprakash@AS-MAC-0234.local>
2022-03-03 17:48:15 +0530

    some editing

--- FunctionalCollections.scala
+++ FunctionalCollections.scala
@@ -1,7 +1,44 @@
+import future.User
+
 object FunctionalCollections extends App {
   /**
    * Set Instances are callable like functions
    * apply method always return true or false
    * Set behave like actual functions
    */
+
+  trait Equal[T] {
+    def apply(a: T, b: T): Boolean
+  }
+
+  implicit object NameEquality extends Equal[User] {
+    override def apply(a: User, b: User): Boolean = a.name == b.name
+  }
+
+  object FullEquality extends Equal[User] {
+    override def apply(a: User, b: User): Boolean = a.name == b.name && a.email == b.email
+  }
+
+  /*
+  Exercise: implement the TC pattern for the Equality tc.
+ */
+  object Equal {
+    def apply[T](a: T, b: T)(implicit equalizer: Equal[T]): Boolean =
+      equalizer.apply(a, b)
+  }
+
+  val john = User("John", 32, "john@rockthejvm.com")
+  val anotherJohn = User("John", 45, "anotherJohn@rtjvm.com")
+  println(Equal(john, anotherJohn))
+  // AD-HOC polymorphism
+
+  /*
+    Exercise - improve the Equal TC with an implicit conversion class
+    ===(anotherValue: T)
+    !==(anotherValue: T)
+   */
+  implicit class TypeSafeEqual[T](value: T) {
+    def ===(other: T)(implicit equalizer: Equal[T]): Boolean = equalizer.apply(value, other)
+    def !==(other: T)(implicit equalizer: Equal[T]): Boolean = ! equalizer.apply(value, other)
+  }
 }
